d00163cb8aff3bfd65d8f1c33eb5f66651b68dbf
darriyano <74460805+Darriyano@users.noreply.github.com>
2024-06-30 22:27:13 +0300

    GET & POST in login page added

--- pages/LoginPage.tsx
+++ pages/LoginPage.tsx
@@ -3,11 +3,47 @@ import '../styles/login.css'
 import {useNavigate} from "react-router-dom";
 
 
+interface LoginData {
+    login: string,
+    password: string
+}
+
 const LoginPage: FC<{ onChange: (value: string) => void }> = ({onChange}) => {
     /* По нажатию будет срабатывать кнопка, которая собирает всю инфу с полей и проверяет ее в handle change, отправляя
     либо пустоту, либо айдишник для послед запросов на бэк*/
-    const handleChange = () => {
-        onChange('user_id_idk')
+    const handleChange = async () => {
+        const login = (document.getElementById('mail') as HTMLInputElement).value;
+        const password = (document.getElementById('password') as HTMLInputElement).value;
+
+        const loginData: LoginData = {
+            login,
+            password
+        }
+
+        try {
+            const sending = await fetch('https://your-backend-url.com/api/signin', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(loginData),
+            });
+            if (!sending.ok) {
+                throw new Error('Network response was not ok');
+            }
+
+            const data = await sending.text();
+
+            if (data == "ok") {
+                onChange(data)
+
+            } else {
+                throw new Error('Theres no such email');
+            }
+        } catch (error) {
+            alert("Error: " + error)
+        }
+
     }
 
     const navigate = useNavigate();
