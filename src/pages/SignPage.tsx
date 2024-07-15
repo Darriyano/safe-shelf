4c67a64acd74b1c736d79c9a592a7233687c8aa3
Дарьяно <dashastepanova1901@mail.ru>
2024-07-15 23:14:25 +0300

    Fixed sign validation

--- src/pages/SignPage.tsx
+++ src/pages/SignPage.tsx
@@ -24,13 +24,23 @@ interface statusResponse400 {
 
 const SignPage = () => {
     const navigate = useNavigate();
+    const [isEmailValid, setIsEmailValid] = useState(true);
+    const [isPwValid, setIsPwValid] = useState(true);
+    const [isNameValid, setIsNameValid] = useState(true);
+    const [isSurnameValid, setIsSurnameValid] = useState(true);
+    const [isAgeValid, setIsAgeValid] = useState(true);
+    const [isHeightValid, setIsHeightValid] = useState(true);
+    const [isWeightValid, setIsWeightValid] = useState(true);
 
-    const validateEmail = (email: string): boolean => {
-        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        return emailRegex.test(email);
-
+    const validateSpaces = (str: string): boolean => {
+        const spaceRegex = /^\S*$/;
+        return spaceRegex.test(str);
     };
 
+    const validateAlph = (str: string): boolean => {
+        const alRegex = /^[A-Za-z]+$/;
+        return alRegex.test(str);
+    };
 
     const handleSignIn = async () => {
         const login = (document.getElementById('email') as HTMLInputElement).value;
@@ -58,12 +68,106 @@ const SignPage = () => {
         }
 
         try {
-            if (!validateEmail(login)) {
+            if (!login || !password || !name || !surname || !age || !height || !weight) {
+                throw new Error("Empty data!");
+            }
+
+            if (!validateSpaces(login) || !validateAlph(login)) {
+                setIsEmailValid(false);
+                const emailInput = document.getElementById('email') as HTMLInputElement;
+                emailInput.value = "Input should contain only english alphabet";
                 throw new Error("ERROR WHILE INPUT LOGIN");
             }
 
-            if (!login || !password || !name || !surname || !age || !height || !weight || height < 100 || weight < 20 || height > 250 || weight > 400 || age < 0 || age > 110) {
-                throw new Error("INCORRECT DATA!");
+            if (!validateSpaces(password)) {
+                setIsEmailValid(true);
+                setIsAgeValid(true);
+                setIsHeightValid(true);
+                setIsSurnameValid(true);
+                setIsWeightValid(true);
+                setIsPwValid(true)
+                setIsNameValid(true);
+
+                setIsPwValid(false);
+                throw new Error("ERROR WHILE INPUT PASSWORD");
+            }
+
+            if (!validateSpaces(name) || name.length >= 30 || !validateAlph(name)) {
+                setIsEmailValid(true);
+                setIsAgeValid(true);
+                setIsHeightValid(true);
+                setIsSurnameValid(true);
+                setIsWeightValid(true);
+                setIsPwValid(true)
+
+                setIsNameValid(false);
+                const nameInput = document.getElementById('name') as HTMLInputElement;
+                if (name.length > 30) {
+                    nameInput.value = "Name should be less than 30 symbols";
+                } else {
+                    nameInput.value = "Input should contain only english alphabet";
+                }
+                throw new Error("ERROR WHILE INPUT NAME");
+            }
+
+            if (!validateSpaces(surname) || surname.length >= 30 || !validateAlph(surname)) {
+                setIsEmailValid(true);
+                setIsAgeValid(true);
+                setIsHeightValid(true);
+                setIsWeightValid(true);
+                setIsPwValid(true)
+                setIsNameValid(true);
+
+                setIsSurnameValid(false);
+                const nameInput = document.getElementById('surname') as HTMLInputElement;
+                if (surname.length > 30) {
+                    nameInput.value = "Name should be less than 30 symbols";
+                } else {
+                    nameInput.value = "Input should contain only english alphabet";
+                }
+                throw new Error("ERROR WHILE INPUT SURNAME");
+            }
+
+            if (weight < 1 || weight > 310) {
+                setIsEmailValid(true);
+                setIsAgeValid(true);
+                setIsHeightValid(true);
+                setIsSurnameValid(true);
+                setIsPwValid(true)
+                setIsNameValid(true);
+
+                setIsWeightValid(false);
+                const weightInput = document.getElementById('weight') as HTMLInputElement;
+                weightInput.value = '';
+                throw new Error("ERROR WHILE INPUT WEIGHT");
+            }
+
+            if (height < 1 || height > 220) {
+                setIsEmailValid(true);
+                setIsAgeValid(true);
+                setIsSurnameValid(true);
+                setIsWeightValid(true);
+                setIsPwValid(true)
+                setIsNameValid(true);
+
+                setIsHeightValid(false);
+                const heightInput = document.getElementById('height') as HTMLInputElement;
+                heightInput.value = '';
+                throw new Error("ERROR WHILE INPUT HEIGHT");
+            }
+
+            if (age < 1 || age > 110) {
+                setIsEmailValid(true);
+                setIsHeightValid(true);
+                setIsSurnameValid(true);
+                setIsWeightValid(true);
+                setIsPwValid(true)
+                setIsNameValid(true);
+
+                setIsAgeValid(false);
+                const ageInput = document.getElementById('age') as HTMLInputElement;
+                ageInput.value = '';
+                throw new Error("ERROR WHILE INPUT AGE");
             }
 
             const sending = await fetch("/account/register", {
@@ -87,36 +191,49 @@ const SignPage = () => {
                 throw new Error('Непредвиденная ошибка. Попробуйте позже');
             }
         } catch (error) {
-            alert("Error: " + error)
+            console.warn(error)
         }
     };
 
     return (
         <div className="wrapper">
             <h1>SafeShelf</h1>
-
             <div className="inputs-sig">
 
                 <label htmlFor="mail">Login</label>
-                <input type="email" id="email" name="login" required/>
+                <input type="email" id="email" name="login" style={{
+                    border: isEmailValid ? 'none' : '2px solid red'
+                }} required/>
 
                 <label htmlFor="pass">Password</label>
-                <input type="password" id="new-password" required/>
+                <input type="password" id="new-password" style={{
+                    border: isPwValid ? 'none' : '2px solid red'
+                }} required/>
 
                 <label htmlFor="name">Name:</label>
-                <input type="text" id="name" name="name" required/>
+                <input type="text" id="name" name="name" style={{
+                    border: isNameValid ? 'none' : '2px solid red'
+                }} required/>
 
                 <label htmlFor="surname">Surname:</label>
-                <input type="text" id="surname" name="surname" required/>
+                <input type="text" id="surname" name="surname" style={{
+                    border: isSurnameValid ? 'none' : '2px solid red'
+                }} required/>
 
                 <label htmlFor="height">Height:</label>
-                <input type="number" id="height" name="height" min="0" max="100" required/>
+                <input type="number" id="height" name="height" min="0" max="100" style={{
+                    border: isHeightValid ? 'none' : '2px solid red'
+                }} required/>
 
                 <label htmlFor="weight">Weight:</label>
-                <input type="number" id="weight" name="weight" min="0" max="100" required/>
+                <input type="number" id="weight" name="weight" min="0" max="100" style={{
+                    border: isWeightValid ? 'none' : '2px solid red'
+                }} required/>
 
                 <label htmlFor="age">Age:</label>
-                <input type="number" id="age" name="age" min="0" max="100" required/>
+                <input type="number" id="age" name="age" min="0" max="100" style={{
+                    border: isAgeValid ? 'none' : '2px solid red'
+                }} required/>
 
                 <label htmlFor="gender">Gender:</label>
                 <select id="gender" name="gender" required>
