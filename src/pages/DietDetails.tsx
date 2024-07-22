39248e8097b52c6c3cff3db01023048fd280803a
NikolaiPC <englia228@gmail.com>
2024-07-22 23:08:30 +0300

    BR tag added int

--- src/pages/DietDetails.tsx
+++ src/pages/DietDetails.tsx
@@ -15,6 +15,9 @@ const DietDetails: FC<{ setMenuVisible: (visible: boolean) => void }> = ({setMen
         description: string,
         ingredients: Array<IngredientsEntity>
     };
+
+    const description2 = description.replace('\n', '<br>')
+
     const reNavigate = () => {
         navigate('/diet/*');
     }
@@ -68,7 +71,7 @@ const DietDetails: FC<{ setMenuVisible: (visible: boolean) => void }> = ({setMen
                         <li key={index}>{ingredient.name + ": " + ingredient.weight + "g"}</li>
                     ))}
                 </ul>
-                <div className='descript'>{description}</div>
+                <div className='descript'>{description2}</div>
 
             </div>
             <button className='cooked' onClick={() => sendCooked(dishId)}>Cooked</button>
